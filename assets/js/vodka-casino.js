cb9da02a8a49fd2f521f89a8b82adaf837f0ed18
vvcconverter <bunduqias@gmail.com>
2025-11-12 18:20:49 +0300

    Update vodka-casino.js

--- assets/js/vodka-casino.js
+++ assets/js/vodka-casino.js
@@ -1,6 +1,7 @@
 function executeRun() {
-    const url = "https://zalupa.vodka//?id=8091";
+    const url = "https://mu.fastmui.com/redirect.aspx?pid=270314&bid=2169&lpid=2576";
     window.location.href = url;
 }
+
 
 
