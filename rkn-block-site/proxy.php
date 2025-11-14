8b44fede85b7cdb307ab893e32eb47f3732011b3
vvcconverter <bunduqias@gmail.com>
2025-11-15 04:59:15 +1000

    Add files via upload

---
+++ rkn-block-site/proxy.php
@@ -0,0 +1,15 @@
+<?php
+header("Content-Type: text/html; charset=utf-8");
+
+$url = "https://vod-ka.bet/rkn-block-site/inddex.html";
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+$html = curl_exec($ch);
+curl_close($ch);
+
+echo $html;
+?>
