10c7c8be6c736d6114a6ba61ee6fa9584fb8ce54
vvcconverter <bunduqias@gmail.com>
2025-11-24 15:33:16 +0300

    Update 1.js

--- icon/1.js
+++ icon/1.js
@@ -1,5 +1,4 @@
 function executeRun() {
-  window.location.href = "https://mu.fastmui.com/redirect.aspx?pid=270314&bid=2169&lpid=2576";
+  window.location.href = "https://casino-app.store/link/spinando.html";
 
 }
-
